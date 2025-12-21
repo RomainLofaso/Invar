c713fb0c33903c0bb2ef1b7f01ba8b6e963d6fa5
Romain Lofaso <romain.lofaso@gmail.com>
2025-12-21 13:08:56 +0100

    Initial Commit

---
+++ ColorInverter/ColorInverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorInverterApp.swift
+//  ColorInverter
+//
+//  Created by Romain Lofaso on 21/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorInverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
